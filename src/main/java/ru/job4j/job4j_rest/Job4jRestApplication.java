8644d374657c6751353a76f128a5bc50f854f57e
xocer <vitamin9292@mail.ru>
2021-11-05 21:53:41 +0300

    Initial commit

---
+++ src/main/java/ru/job4j/job4j_rest/Job4jRestApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.job4j_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jRestApplication.class, args);
+    }
+
+}
